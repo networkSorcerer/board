de72ae848cbc533f22c825a3156e647fd53ec9ca
networdSorserer <gigattfyhig@gmail.com>
2024-05-24 17:27:15 +0900

    board

--- JavaWeb/src/main/java/com/spring/user/dao/UserDao.java
+++ JavaWeb/src/main/java/com/spring/user/dao/UserDao.java
@@ -1,5 +1,31 @@
 package com.spring.user.dao;
 
-public interface UserDao {
+import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
+
+import com.spring.user.vo.UserVO;
+
+@Mapper
+public interface UserDao {
+	public UserVO userLoginProcess(UserVO uvo);
+	
+	public UserVO findId(UserVO uvo);
+	public int findUserByIdAndEmail(UserVO uvo);
+	public int resetPasswd(UserVO uvo);
+	
+	public int userJoin(UserVO uvo);
+	public int idCheck(UserVO uvo);
+	public int phoneCheck(UserVO uvo);
+	public int emailCheck(UserVO uvo);
+	
+	public UserVO userInfo(UserVO uvo);
+	public int updateProfile(UserVO uvo);
+	public int pwdConfirm(UserVO uvo);
+	
+	public int userWithdrawal(UserVO uvo);
+	public int userDelete(UserVO uvo);
+	
+	public List<UserVO> userList(UserVO uvo);
+	public int userListCnt(UserVO uvo);
 }
