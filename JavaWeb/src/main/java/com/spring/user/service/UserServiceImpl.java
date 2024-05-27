28b2a0e1c85d45845c2ae390f1fe479b3f2fdb72
networkSorcerer <gigattfyhig@gmail.com>
2024-05-27 17:43:34 +0900

    board

--- JavaWeb/src/main/java/com/spring/user/service/UserServiceImpl.java
+++ JavaWeb/src/main/java/com/spring/user/service/UserServiceImpl.java
@@ -3,9 +3,13 @@ package com.spring.user.service;
 import java.security.SecureRandom;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.spring.user.dao.UserDao;
 import com.spring.user.vo.UserVO;
+
+import lombok.Setter;
 
 @Service
 public class UserServiceImpl implements UserService{
@@ -13,10 +17,14 @@ public class UserServiceImpl implements UserService{
 	private static final char [] CHAR_SET =  "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".toCharArray();
 	private static final SecureRandom random = new SecureRandom();
 	private static int PASSWORD_LENGTH = 10;
+	
+	@Setter(onMethod_=@Autowired)
+	private UserDao userDao;
+	
 	@Override
 	public UserVO userLoginProcess(UserVO uvo) {
-		// TODO Auto-generated method stub
-		return null;
+		UserVO result = userDao.userLoginProcess(uvo);
+		return result;
 	}
 	@Override
 	public UserVO findId(UserVO uvo) {
